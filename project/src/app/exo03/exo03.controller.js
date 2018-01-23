ee6797311ecbefc06b26e85ee1ab8397bdba749b
PaulLAGUES <paul.lagues@epsi.fr>
2018-01-23 17:48:27 +0100

    reste que exo08 a faire

--- project/src/app/exo03/exo03.controller.js
+++ project/src/app/exo03/exo03.controller.js
@@ -5,7 +5,26 @@
     var vm = this;
     vm.msg = 'Welcome All!';
     vm.subMsg = 'A seed Project';
+    vm.films = [
+    {name: 'Fast and Furious', img: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/AkSMd8AXSpA1L1SbBPc2AfVJfmE.jpg', year: 2001},
+    {name: '2 Fast 2 Furious', img: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/u1Snfl40rXmAJNiMyZUczUyFVuf.jpg', year: 2003},
+    {name: 'Fast & Furious: Tokyo Drift', img: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/4ceW7kWPAc2uFG5WzYDbP8Od3gw.jpg', year: 2006}
+    ];
+
+
+    vm.clickImg = clickImg;
+    var idFilms;
+    vm.idFilms = null;
+    function clickImg (index){
+      vm.idFilms = vm.films[index];
+      console.log(idFilms.name);
+    }
   }
+
+
+
+ 
+
 
   exo03Ctrl.$inject = [];
 
